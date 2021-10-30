d57a4bea330585de0609d0aa1e326e20b59f0e74
Johan Aguilar <aguilarv@mit.edu>
2021-10-30 15:57:48 -0400

    Added all black website

--- pongGame.js
+++ pongGame.js
@@ -1,2 +1,6 @@
-const testVar = "HelloWorld!!";
-console.log(testVar);
+let cnv = document.getElementById("cnv");
+let ctx = cnv.getContext("2d");
+//cnv.width = window.innerWidth;
+//cnv.height = window.innerHeight;
+ctx.fillStyle = 'white';
+ctx.fillRect(25,75, 25,400);
